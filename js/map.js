a63c240983c58f7264a4fda27fe484b1d898e23f
roocheerah <ruchira.kulk@gmail.com>
2015-11-23 01:19:28 -0800

    fixing the order of the imports

--- js/map.js
+++ js/map.js
@@ -1,10 +1,9 @@
 "use strict"
-window.onload = function(){
-	var map;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map-canvas'), {
-        center: { lat: 47.6550, lng: -122.3080},
-        zoom: 15
-      });
-    }
-};
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: { lat: 47.6550, lng: -122.3080},
+    zoom: 15
+  });
+}
